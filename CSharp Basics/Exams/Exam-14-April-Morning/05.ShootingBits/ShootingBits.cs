8d303fb756f3edb5ffa01bd24b979c7fac19bc49
Spas Vutov <VutovSpas@gmail.com>
2014-11-04 17:09:38 +0200

    fix
    exam 14 April evening 05 problem

--- CSharp Basics/Exams/Exam-14-April-Morning/05.ShootingBits/ShootingBits.cs
+++ CSharp Basics/Exams/Exam-14-April-Morning/05.ShootingBits/ShootingBits.cs
@@ -4,48 +4,51 @@ class BitShooter
 {
     static void Main()
     {
-        const int BITS = 64;
+        //ulong number = 4227378815862876842;
+        //ulong number = 33860256;
+        ulong number = ulong.Parse(Console.ReadLine());
 
-        ulong inputBits = ulong.Parse(Console.ReadLine());
-        ulong shootedBits = 0;
-        for (int i = 0; i < 3; i++)
+        for (int shoots = 0; shoots < 3; shoots++)
         {
-            string shoot = Console.ReadLine();
-            string[] shootDetails = shoot.Split(' ');
-            int shootCenter = int.Parse(shootDetails[0]);
-            int shootSize = int.Parse(shootDetails[1]);
-            int startBit = shootCenter - shootSize / 2;
-            int endBit = shootCenter + shootSize / 2;
-            for (int bit = startBit; bit <= endBit; bit++)
+            string input = Console.ReadLine();
+            //string input = "22 3";
+            int[] coordinates = Array.ConvertAll(input.Split(' '), int.Parse);
+            int centre = coordinates[0];
+            int size = coordinates[1];
+            int halfSize = size / 2;
+            int startBit = centre - halfSize;
+            int endBit = centre + halfSize;
+            //Set bits to 0;
+            for (int bit = startBit; bit <= endBit; bit++) //All blasted bits;
             {
-                if (bit >= 0 && bit < BITS)
+                if (bit >= 0 && bit < 64)
                 {
-                    shootedBits = shootedBits | ((ulong)1 << bit);
+                    number = number & ~(1ul << bit);
                 }
             }
+            //Console.WriteLine(number);
         }
-
-        ulong aliveBits = inputBits & (~shootedBits);
-
-        //Console.WriteLine(Convert.ToString((long)inputBits, 2).PadLeft(64,'0'));
-        //Console.WriteLine(Convert.ToString((long)~shootedBits, 2).PadLeft(64, '0'));
-        //Console.WriteLine(Convert.ToString((long)aliveBits, 2).PadLeft(64, '0'));
-
-        ulong rightBits = 0;
-        for (int i = 0; i < BITS / 2; i++)
+        //Console.WriteLine(number);
+        int leftBitsCount = 0;
+        int rightBitsCount = 0;
+        for (int bit = 0; bit <= 31; bit++)//Right bits;
         {
-            rightBits += aliveBits & 1;
-            aliveBits >>= 1;
+            ulong currentBit = (number >> bit) & 1;
+            if (currentBit == 1)
+            {
+                rightBitsCount++;
+            }
         }
-
-        ulong leftBits = 0;
-        for (int i = 0; i < BITS / 2; i++)
+        //Console.WriteLine(rightBitsCount);
+        for (int bit = 32; bit <= 63; bit++)
         {
-            leftBits += aliveBits & 1;
-            aliveBits >>= 1;
+            ulong currentBit = (number >> bit) & 1;
+            if (currentBit == 1)
+            {
+                leftBitsCount++;
+            }
         }
+        Console.WriteLine("left: {0}\nright: {1}",leftBitsCount, rightBitsCount);
 
-        Console.WriteLine("left: " + leftBits);
-        Console.WriteLine("right: " + rightBits);
     }
 }
