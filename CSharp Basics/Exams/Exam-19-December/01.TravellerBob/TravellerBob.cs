31945f8581a162d5d04a27081788e6f8dac2536b
Spas Vutov <VutovSpas@gmail.com>
2014-12-24 14:56:18 +0200

    Add
    Exam 19 Dec 1/5

---
+++ CSharp Basics/Exams/Exam-19-December/01.TravellerBob/TravellerBob.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class TravellerBob
+{
+    static void Main(string[] args)
+    {
+        string year = Console.ReadLine();
+        int contractMonths = int.Parse(Console.ReadLine());
+        int familyMonths = int.Parse(Console.ReadLine());
+
+        int weeksPerMonth = 4;
+        double contractTravels = contractMonths * (weeksPerMonth * 3);
+        //Console.WriteLine(contractTravels);
+        double familyTravels = familyMonths * 4; //2 weeks with 3 - 1 travels;
+        //Console.WriteLine(familyTravels);
+        double normalTravels = (12 - contractMonths - familyMonths) * (12 * 3 / 5d);
+        //Console.WriteLine(normalTravels);
+        double totalTravels = contractTravels + familyTravels + normalTravels;
+        if (year == "leap")
+        {
+            totalTravels *= 1.05d;
+        }
+
+        Console.WriteLine((int) totalTravels);
+    }
+}
