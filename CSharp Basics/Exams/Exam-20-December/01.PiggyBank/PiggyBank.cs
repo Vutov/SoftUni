2972f68f04103c74afc58d63077ba143c1715056
Spas Vutov <VutovSpas@gmail.com>
2014-12-26 03:26:45 +0200

    Add
    C# Exam 20 Dec 2/5

---
+++ CSharp Basics/Exams/Exam-20-December/01.PiggyBank/PiggyBank.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class PiggyBank
+{
+    static void Main(string[] args)
+    {
+        int tankPrice = int.Parse(Console.ReadLine());
+        int partyDays = int.Parse(Console.ReadLine());
+
+        int monthDays = 30;
+        int yearMonths = 12;
+        int normalDays = monthDays - partyDays;
+        double savedMoney = normalDays * 2d - (partyDays * 5d);
+        if (savedMoney > 0)
+        {
+            double totalMonths = tankPrice / savedMoney;
+            double neededMonths = Math.Ceiling(totalMonths % yearMonths);
+            int neededYears = (int)(totalMonths / yearMonths);
+            if (neededMonths == 12)
+            {
+                neededMonths = 0;
+                neededYears++;
+            }
+            Console.WriteLine("{0} years, {1} months", neededYears, neededMonths);
+        }
+        else
+        {
+            Console.WriteLine("never");
+        }
+
+
+
+    }
+}
---
+++ CSharp Basics/Exams/Exam-20-December/02.CheatSheet/CheatSheet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class CheatSheet
+{
+    static void Main(string[] args)
+    {
+        int rowSize = int.Parse(Console.ReadLine());
+        int colSize = int.Parse(Console.ReadLine());
+        long startRow = int.Parse(Console.ReadLine());
+        long startCol = int.Parse(Console.ReadLine());
+
+        for (int row = 0; row < rowSize; row++)
+        {
+            for (int col = 0; col < colSize; col++)
+            {
+                long currentNum = startRow * startCol;
+                if (col == 0)
+                {
+                    Console.Write("{0}", currentNum);
+                }
+                else
+                {
+                    Console.Write(" {0}", currentNum);
+                }
+                startCol++;
+            } 
+            Console.WriteLine();
+            startCol -= colSize;
+            startRow++;
+        }
+    }
+}
