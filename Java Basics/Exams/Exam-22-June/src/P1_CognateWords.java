8690e07ccef72ef81ba4070470e57fed637a559e
Spas Vutov <VutovSpas@gmail.com>
2015-01-25 13:31:43 +0200

    Exam-22-June P1 fixed

--- Java Basics/Exams/Exam-22-June/src/P1_CognateWords.java
+++ Java Basics/Exams/Exam-22-June/src/P1_CognateWords.java
@@ -1,32 +1,26 @@
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class P1_CognateWords {
-	
-	//Need debug zero test 3
-	
-	
+
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		boolean found = false;
-		HashMap<String, Integer> foundCouples = new HashMap<String, Integer>();
+		Set<String> foundCouples = new HashSet<String>();
 		// String data = "java..?|basics/*-+=javabasics";
 		String data = input.nextLine();
-		String[] words = data.split("\\W+");
-		for (int i = 0; i < words.length - 1; i++) {
+		String[] words = data.split("([^a-zA-Z])+");
+		for (int i = 0; i < words.length; i++) {
 			for (int j = 0; j < words.length; j++) {
 				for (int k = 0; k < words.length; k++) {
 					String nextWords = words[i] + words[j];
-					if (nextWords.compareTo(words[k]) == 0
-							&& words[j] != words[i]) {
-						if (foundCouples.containsKey(nextWords)) {
-							foundCouples.put(nextWords, 2);
-						} else {
-							foundCouples.put(nextWords, 1);
-							System.out.printf("%s|%s=%s\n", words[i], words[j],
-									words[k]);
-						}
+					if (nextWords.compareTo(words[k]) == 0 && j != i
+							&& !foundCouples.contains(nextWords)) {
+						System.out.printf("%s|%s=%s\n", words[i], words[j],
+								words[k]);
+						foundCouples.add(nextWords);
 						found = true;
 					}
 				}
