a81845e51ca970a8004455b750a823bf029eba89
Spas Vutov <VutovSpas@gmail.com>
2015-03-13 00:23:19 +0200

    JS Exam-27-July P1

---
+++ JavaScript Basics/Exams/Exam-27-July/01.BuildTable/BuildTable.js
@@ -0,0 +1,32 @@
+﻿function Solve(arr) {
+    var start = Number(arr[0]);
+    var end = Number(arr[1]);
+
+    function isFib(val) {
+        var prev = 0;
+        var curr = 1;
+        while (prev <= val) {
+            if (prev === val) {
+                return "yes";
+            }
+            curr = prev + curr;
+            prev = curr - prev;
+        }
+
+        return "no";
+    }
+
+    var result = "<table>\n<tr><th>Num</th><th>Square</th><th>Fib</th></tr>\n";
+    for (var i = start; i <= end; i++) {
+        result += "<tr><td>" + i + "</td><td>" + Math.pow(i, 2) + "</td><td>" + isFib(i) + "</td></tr>\n";
+
+    }
+    result += "</table>";
+    //console.log(result);
+    return result;
+}
+
+//Solve([2, 6]);
+//Solve([55, 56]);
+
+//http://justindavis.co/2014/10/30/determine-if-a-number-is-in-the-fibonacci-series-using-javascript/
