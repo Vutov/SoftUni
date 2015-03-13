88bd05f14a51467bd097e99e7562b6cbb04d9e84
Spas Vutov <VutovSpas@gmail.com>
2015-03-13 19:33:51 +0200

    JS Exam-27-July done

---
+++ JavaScript Basics/Exams/Exam-27-July/02.RevealTriangles/RevealTriangles.js
@@ -0,0 +1,58 @@
+﻿function solve(arr) {
+    var lines = arr.length;
+    //Check every possible little triangle;
+    for (var i = 0; i < lines - 1; i++) {
+        for (var row = 1; row < arr[i].length; row++) {
+            var upper = arr[i].charAt(row);
+            var lower = arr[i + 1].substring(row - 1, row + 2);
+            if (areSame(upper, lower)) {
+                //Mark found combinations;
+                arr[i] = replaceAt(arr[i], row, upper.toUpperCase());
+                arr[i + 1] = replaceAt(arr[i + 1], row - 1, upper.toUpperCase());
+                arr[i + 1] = replaceAt(arr[i + 1], row, upper.toUpperCase());
+                arr[i + 1] = replaceAt(arr[i + 1], row + 1, upper.toUpperCase());
+            }
+        }
+        //console.log(arr[i]);
+    }
+    //console.log(arr[arr.length - 1]);
+
+    //Replace with * and print;
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = replaceUpper(arr[i]);
+         console.log(arr[i]);
+    }
+    
+    function replaceAt(line, index, character) {
+        return line.substr(0, index) + character + line.substr(index + character.length);
+    }
+    
+    function areSame(sample, line) {
+        sample = sample.toLowerCase();
+        line = line.toString().toLowerCase();
+        if (sample.charAt(0) === line.charAt(0) && sample.charAt(0) === line.charAt(1) && sample.charAt(0) === line.charAt(2)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function replaceUpper(line, ch) {
+        for (var j = 0; j < line.length; j++) {
+            var ch = line.charAt(j);
+            if (ch >= 'A' && ch <= 'Z') {
+                line = replaceAt(line, j, '*');
+            }
+        }
+        return line;
+    }
+
+}
+
+//solve(['abcdexgh', 'bbbdxxxh', 'abcxxxxx']);
+//solve([
+//    'aa',
+//    'aaa',
+//    'aaaa ',
+//    'aaaaa ',
+//]);
---
+++ JavaScript Basics/Exams/Exam-27-July/03.ExtractHyperlinks/ExtractHyperlinks.js
@@ -0,0 +1,22 @@
+﻿function solve(arr) {
+    var data = arr.join("\n");
+    var regex = /<a\s+(?:[^>]*)href[\s=]+(?:"([^"]*)"|'([^']*)'|([^\s>]*))[^>]*>/g;
+    //var match = regex.exec(data);
+    var match;
+    while (match = regex.exec(data)) {
+        var href = match[1];
+        if (href === undefined) {
+            var href = match[2];
+        }
+        if (href === undefined) {
+            var href = match[3];
+        }
+        console.log(href);
+    }
+    //console.log(match[1]);
+
+}
+
+//solve(['<a href="http://softuni.bg" class="new"></a>']);
+//solve(['<p>This text has no links</p>']);
+//solve(['<!DOCTYPE html>','<html>','<head>','<title>Hyperlinks</title>','<link href="theme.css" rel="stylesheet" />','</head>','<body>','<ul><li><a   href="/"  id="home">Home</a></li><li><a','class="selected" href=/courses>Courses</a>','</li><li><a href = ','\'/forum\' >Forum</a></li><li><a class="href"','onclick="go()" href= "#">Forum</a></li>','<li><a id="js" href =','"javascript:alert(\'hi yo\')" class="new">click</a></li>','<li><a id=\'nakov\' href =','http://www.nakov.com class=\'new\'>nak</a></li></ul>','<a href="#empty"></a>','<a id="href">href=\'fake\'<img src=\'http://abv.bg/i.gif\' ','alt=\'abv\'/></a><a href="#">&lt;a href=\'hello\'&gt;</a>','<!-- This code is commented:','<a href="#commented">commentex hyperlink</a> -->','</body>']);
