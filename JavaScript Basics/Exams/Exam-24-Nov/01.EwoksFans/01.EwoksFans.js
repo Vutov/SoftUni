f46b075952ec4b40f2745b2425836a285432f261
Spas Vutov <VutovSpas@gmail.com>
2015-03-27 20:01:12 +0200

    Exam-24-Nov all done P4 at 85%, P3 same as 22-Nov

---
+++ JavaScript Basics/Exams/Exam-24-Nov/01.EwoksFans/01.EwoksFans.js
@@ -0,0 +1,48 @@
+﻿function solve(arr) {
+    var biggestFan = [new Date(1973, 04, 25), false];
+    var biggestHater = [new Date(1973, 04, 25), false];
+    arr.forEach(function (x) {
+        var data = x.split('.');
+        var date = new Date(data[2], Number(data[1]) - 1, data[0]);
+        //console.log(new Date())
+        if (date > new Date(1900, 0, 1) && date < new Date(2015, 0, 1)) {
+            //hater
+            //console.log(date)
+            //console.log(new Date(1973, 04, 25))
+            if (date < new Date(1973, 04, 25)) {
+                // console.log(date)
+                if (date < biggestHater[0]) {
+                    biggestHater[0] = date;
+                    biggestHater[1] = true;
+                }
+            } else {
+                //fans
+                if (date > biggestFan[0]) {
+                    biggestFan[0] = date;
+                    biggestFan[1] = true;
+                }
+            }
+        }
+        
+        
+    });
+    if (biggestFan[1]) {
+        console.log('The biggest fan of ewoks was born on ' + biggestFan[0].toDateString());
+    }
+    if (biggestHater[1]) {
+        console.log('The biggest hater of ewoks was born on ' + biggestHater[0].toDateString());
+    }
+    if (!biggestFan[0] && !biggestHater[0]) {
+        console.log('No result');
+    }
+
+}
+
+solve([
+    '22.03.2014',
+    '17.05.1933',
+    '10.10.1954'
+]);
+
+//younges
+//olders
---
+++ JavaScript Basics/Exams/Exam-24-Nov/02.PlusRemove/02.PlusRemove.js
@@ -0,0 +1,37 @@
+﻿function solve(arr) {
+    var longest = 0;
+    var result = [];
+    arr = arr.map(function (line) {
+        result.push(line.split(''));
+        if (line.length > longest) {
+            longest = line.length;
+        }
+        return line.toLowerCase();
+    });
+    for (var l = 0; l < arr.length - 2; l++) {
+        for (var r = 1; r < longest; r++) {
+            if (arr[l][r] === arr[l + 1][r] &&
+                arr[l][r] === arr[l + 1][r + 1] &&
+                arr[l][r] === arr[l + 1][r - 1] &&
+                arr[l][r] === arr[l + 2][r]
+                ) {
+                result[l][r] = '';
+                result[l + 1][r] = '';
+                result[l + 1][r + 1] = '';
+                result[l + 1][r - 1] = '';
+                result[l + 2][r] = '';
+            }
+        }
+    }
+    result.forEach(function(line) {
+        console.log(line.join(''));
+    });
+}
+
+solve([
+    'ab**l5',
+    'bBb*555',
+    'absh*5',
+    'ttHHH',
+    'ttth'
+]);
