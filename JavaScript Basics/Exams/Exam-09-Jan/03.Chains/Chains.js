07936fd2e3d75b13ace8772a668ad4ec4f2fd757
Spas Vutov <VutovSpas@gmail.com>
2015-03-31 00:16:56 +0300

    Exam-09-Jan Done

---
+++ JavaScript Basics/Exams/Exam-09-Jan/03.Chains/Chains.js
@@ -0,0 +1,26 @@
+﻿function solve(arr) {
+    var match;
+    var regex = /<p>(.+?)<\/p>/g;
+    var decripted = "";
+    while (match = regex.exec(arr[0])) {
+        decripted += match[1];
+    }
+    decripted = decripted.replace(/[^a-z0-9\\s]+/g, ' ').replace(/\\s+/g);
+    decripted = decripted.split('');
+    decripted = decripted.map(function(char) {
+        var charNum = char.charCodeAt(0);
+        //console.log(charNum)
+        if (charNum >= 97 && charNum < 110) {
+            return String.fromCharCode(charNum + 13);
+        }
+        else if (charNum >= 110 && charNum <= 122) {
+            return String.fromCharCode(charNum - 13);
+        } else {
+            return char;
+        }
+    });
+    console.log(decripted.join(''));
+}
+
+solve(["<html><head><title></title></head><body><h1>hello</h1><p>znahny!@#%&&&&****</p><div><button>dsad</button></div><p>grkg^^^^%%%)))([]12</p></body></html>"]);
+solve(["<html><head><title></title></head><body><h1>hello</h1><p>znahny!@#%&&&&****</p><div><button>dsad</button></div><p>grkg^^^^%%%)))([]12</p></body></html>"]);
---
+++ JavaScript Basics/Exams/Exam-09-Jan/04.Luggage/Luggage.js
@@ -0,0 +1,102 @@
+﻿function solve(arr) {
+    var json = {};
+    for (var i = 0; i < arr.length - 1; i++) {
+        var data = arr[i].split(/\.*\*\.*/g);
+        //console.log(data);
+        var name = data[0];
+        var lugName = data[1];
+        var isFood = data[2];
+        var isDrink = data[3];
+        var isFragile = data[4];
+        var weight = Number(data[5]);
+        var transferWith = data[6];
+        var type = 'other';
+        if (isFood === 'true') {
+            type = 'food';
+        }
+        if (isDrink === 'true') {
+            type = 'drink';
+        }
+        if (isFragile === 'true') {
+            isFragile = true;
+        } else {
+            isFragile = false;
+        }
+        if (!json[name]) {
+            json[name] = {};
+        }
+        if (!json[name][lugName]) {
+            json[name][lugName] = { kg: weight, fragile: isFragile, type: type, transferredWith: transferWith };
+        }
+        json[name][lugName] = { kg : weight, fragile: isFragile, type : type, transferredWith: transferWith };
+    }
+    var sortType = arr[arr.length - 1];
+    if (sortType === 'strict') {
+        console.log(JSON.stringify(json));
+    }
+    else if (sortType === 'luggage name') {
+        for (var name in json) {
+            json[name] = sortObj(json[name]);
+        }
+        console.log(JSON.stringify(json));
+    }
+    else { //Weight
+        for (var name in json) {
+                json[name]= sortObjByWeight(json[name]);
+        }
+        console.log(JSON.stringify(json));
+    }
+    
+    //console.log(json);
+    
+    function sortObj(arr) {
+        // Setup Arrays
+        var sortedKeys = new Array();
+        var sortedObj = {};
+        
+        // Separate keys and sort them
+        for (var i in arr) {
+            sortedKeys.push(i);
+        
+        }
+        sortedKeys.sort();
+        
+        // Reconstruct sorted obj based on keys
+        for (var i in sortedKeys) {
+            sortedObj[sortedKeys[i]] = arr[sortedKeys[i]];
+            
+        }
+        return sortedObj;
+       
+    }
+    
+    function sortObjByWeight(arr) {
+        // Setup Arrays
+        var sortedKeys = new Array();
+        var sortedObj = {};
+        // Separate obj in array and sort them
+        for (var i in arr) {
+            arr[i]['key'] = i;
+            sortedKeys.push(arr[i]);
+        }
+        sortedKeys.sort(function (x, y) {
+            return x['kg'] - y['kg'];
+        });
+        
+        // Reconstruct sorted obj based on keys
+        for (var i in sortedKeys) {
+            sortedObj[sortedKeys[i]['key']] = sortedKeys[i];
+            delete sortedKeys[i]['key'];
+        }
+        return sortedObj;
+       
+    }
+}
+
+//solve([
+//    'Kiko.*.socks.*.false.*.false.*.false.*.0.2.*.backpack',
+//    'Kiko.*.banana.*.true.*.false.*.false.*.3.2.*.backpack',
+//    'Kiko.*.sticks.*.false.*.false.*.false.*.1.6.*.ATV',
+//    'Kiko.*.glasses.*.false.*.false.*.true.*.3.*.ATV',
+//    'weight'
+//]);
