43e4584b2f03d5a9f7b9a3d7d99624a3fab680fd
Spas Vutov <VutovSpas@gmail.com>
2015-03-30 16:38:32 +0300

    Exam-09-Jan 1/2 Done

---
+++ JavaScript Basics/Exams/Exam-09-Jan/01.TetevenTrip/TetevenTrip.js
@@ -0,0 +1,34 @@
+﻿function solve(arr) {
+    arr.forEach(function(line) {
+        var data = line.split(/\s+/g);
+        var car = data[0];
+        var fuel = data[1];
+        var route = data[2];
+        var kg = data[3];
+        var fuelPerHun = 10;
+        if (fuel === 'gas') {
+            fuelPerHun *= 1.2;
+        }
+        else if (fuel === 'diesel') {
+            fuelPerHun *= 0.8;
+        }
+        var extraFuel = kg * 0.01;
+        fuelPerHun += extraFuel;
+        var totalFuel = 0;
+        if (route === '1') {
+            totalFuel += fuelPerHun;
+            totalFuel += (fuelPerHun * 10 / 100) * 1.3;
+        } else {
+            totalFuel += fuelPerHun * 65 / 100;
+            totalFuel += (fuelPerHun * 30 / 100) * 1.3;
+        }
+        console.log(car + " " + fuel +" " + route +" " + Math.round(totalFuel));
+    });
+}
+
+solve([
+    'BMW petrol 1 320.5',
+    'Golf petrol 2 150.75',
+    'Lada gas 1 202',
+    'Mercedes diesel 2 312.54'
+]);
---
+++ JavaScript Basics/Exams/Exam-09-Jan/02.Parachute/Parachute.js
@@ -0,0 +1,79 @@
+﻿function solve(arr) {
+    var pos = 0;
+    //arr.forEach(function(x) { console.log(x) });
+    var parRow = 0;
+    for (var l = 0; l < arr.length; l++) {
+        var firstLine = arr[l].split('');
+        for (var k = 0; k < firstLine.length; k++) {
+            if (firstLine[k] === 'o') {
+                pos = k;
+                parRow = l;
+            }
+        }
+    }
+    
+    for (var j = parRow +1; j < arr.length; j++) {
+        var moveLeft = 0;
+        var moveRight = 0;
+        var ch = arr[j].split('');
+        for (var i = 0; i < ch.length; i++) {
+            if (ch[i] === '<') {
+                moveLeft++;
+            } else if (ch[i] === '>') {
+                moveRight++;
+            }
+        }
+        pos += (moveRight - moveLeft);
+        //console.log(pos);
+        if (ch[pos] === '_') {
+            //console.log(ch[pos]);
+            console.log('Landed on the ground like a boss!');
+            console.log(j + ' ' + pos);
+            break;
+        } 
+        else if (ch[pos] === '~') {
+            //console.log(ch[pos]);
+            console.log('Drowned in the water like a cat!');
+            console.log(j + ' ' + pos);
+            break;
+        } 
+        else if (
+            ch[pos] === '/' || 
+            ch[pos] === '\\' || 
+            ch[pos] === '|'
+            ) {
+            //console.log(ch[pos]);
+            console.log('Got smacked on the rock like a dog!');
+            console.log(j + ' ' + pos);
+            break;
+        }
+    }
+
+}
+
+
+//solve([
+//    '-------------o-<<--------',
+//    '-------->>>>>------------',
+//    '---------------->-<---<--',
+//    '------<<<<<-------/\\--<--',
+//    '--------------<--/-<\\----',
+//    '>>--------/\\----/<-<-\\---',
+//    '---------/<-\\--/------\\--',
+//    '<-------/----\\/--------\\-',
+//    '\------/--------------<-\\',
+//    '-\___~/------<-----------'
+//]);
+
+solve([
+    '--o----------------------',
+    '>------------------------',
+    '>------------------------',
+    '>-----------------/\\-----',
+    '-----------------/--\\----',
+    '>---------/\\----/----\\---',
+    '---------/--\\--/------\\--',
+    '<-------/----\\/--------\\-',
+    '\\------/----------------\\',
+    '-\\____/------------------'
+]);
