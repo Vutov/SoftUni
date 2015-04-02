0457eed524a28a8e00f041ed579e299f2bb816fd
Spas Vutov <VutovSpas@gmail.com>
2015-04-02 22:17:59 +0300

    Lab-14-Nov

---
+++ JavaScript Basics/Exams/Lab-14-nov/AutoComplete/app.js
@@ -0,0 +1,36 @@
+﻿function solve(arr) {
+    var text = arr[0].split(' ');
+    arr.shift(0);
+    arr.forEach(function (x) {
+        var matchingWords = [];
+        text.forEach(function (word) {
+            //console.log(word.slice(0, x.length));
+            if (word.toLowerCase().slice(0, x.length) === x.toLowerCase()) {
+                matchingWords.push(word);
+            }
+        });
+        matchingWords.sort(function(x, y) {
+            if (x.length === y.length) {
+                //console.log("1");
+                return x.toLowerCase().localeCompare(y.toLowerCase());
+            } else {
+                return x.length - y.length;
+            }
+        });
+        if (matchingWords.length !== 0) {
+            console.log(matchingWords[0]);
+        } else {
+            console.log('-');
+        }
+        //console.log(matchingWords);
+        //console.log('--');
+    });
+}
+
+solve([
+    'word screammm screech speech wolf',
+    'bas',
+    'wo',
+    'scr',
+    's'
+]);
