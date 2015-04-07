fc6328926f4cf7e63603911bf5f1a5e1ad13d824
Vutov <VutovSpas@gmail.com>
2015-04-07 17:08:56 +0300

    Added php folder, linux

---
+++ PHP/Homeworks/01.InstallAndConfigure/Numbers.php
@@ -0,0 +1,15 @@
+<?php header('Content-type: text/html; charset=utf-8;') ?>
+<?php
+
+//phpinfo();
+for ($i = 0; $i < 100; $i++) {
+    echo "$i </br>";
+}
+
+echo 1;
+echo 1;
+echo 1;
+echo 1;
+echo 1;
+echo 1;
+?>
