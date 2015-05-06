57c79238c5ca10e0523f7b4b807642766a817d19
Spas Vutov <VutovSpas@gmail.com>
2015-05-06 15:56:47 +0300

    C#A 01.Homework P07 Added

--- CSharp Advanced/Homeworks/01.Arrays, Lists, Stacks, Queues/07.SortedSubsetSums/SortedSubsetSums.cs
+++ CSharp Advanced/Homeworks/01.Arrays, Lists, Stacks, Queues/07.SortedSubsetSums/SortedSubsetSums.cs
@@ -4,30 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-class SubsetSums
+class SortedSubsetSums
 {
     static void Main(string[] args)
     {
         int neededSum = int.Parse(Console.ReadLine());
         List<int> input = Console.ReadLine().Split(' ').Select(x => int.Parse(x)).Distinct().ToList();
 
-        List<string> allLists = getAllCombinations(input, neededSum);
+        List<List<int>> allLists = getAllCombinations(input, neededSum);
+        allLists = sortCombinations(allLists);
         if (allLists.Count == 0)
         {
             Console.WriteLine("No matching subsets.");
         }
         foreach (var item in allLists)
         {
-            Console.WriteLine("{0} = {1}", item, neededSum);
+            Console.WriteLine("{0} = {1}", string.Join(" + ", item), neededSum);
         }
 
     }
-    private static List<String> getAllCombinations(List<int> list, int neededSum)
+    private static List<List<int>> getAllCombinations(List<int> list, int neededSum)
     {
         //Used:
         //http://stackoverflow.com/questions/7802822/all-possible-combinations-of-a-list-of-values-in-c-sharp
-        List<string> allLists = new List<string>();
+        List<List<int>> allLists = new List<List<int>>();
         List<int> currList = new List<int>();
         double count = Math.Pow(2, list.Count);
         for (int i = 1; i <= count - 1; i++)
@@ -40,13 +40,33 @@ class SubsetSums
                     currList.Add(list[j]);
                 }
             }
+
             if (currList.Sum() == neededSum)
             {
-                allLists.Add(String.Join(" + ", currList));
+                allLists.Add(new List<int>(currList));
             }
+
             currList.Clear();
         }
+
         return allLists;
     }
-}
 
+    private static List<List<int>> sortCombinations(List<List<int>> list)
+    {
+        list.Sort((x, y) =>
+        {
+            x.Sort();
+            y.Sort();
+            if (x.Count == y.Count)
+            {
+                return x[0] - y[0];
+            }
+
+            return x.Count - y.Count;
+
+        });
+
+        return list;
+    }
+}
