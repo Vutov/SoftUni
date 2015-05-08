591fb72431eed895ef189e5f72bde36186b60deb
Spas Vutov <VutovSpas@gmail.com>
2015-05-08 23:50:13 +0300

    HQC Rest of 02.Refactoring homework

--- HQC/Practice/02. Code-Formatting-Homework/Manager.php
+++ HQC/Practice/02. Code-Formatting-Homework/Manager.php
@@ -1,45 +1,49 @@
 <?php namespace League\CLImate\Settings;
 
 class Manager {
-    protected
-	$settings;
-	$settings = [];
-    public 
-	function 
-	exists($name)
+    protected $settings = [];
+	
+    public function exists($name)
     {
-	
-	
-        return 
-		class_exists($this->getPath($name));
+        return class_exists($this->getPath($name));
     }
 
-    public function add($name, $value){       $setting = $this->getPath($name); $key     = $this->getClassName($name);if (!array_key_exists($name, $this->settings)) {
-            $this->settings[$key] = new $setting();
-        } $this->settings[$key]->add($value);
+    public function add($name, $value){ 
+		$setting = $this->getPath($name); 
+		$key = $this->getClassName($name);
+		if (!array_key_exists($name, $this->settings)) {
+				$this->settings[$key] = new $setting();
+			} 
+		
+		$this->settings[$key]->add($value);
     }
 
     public function get($key)
     {
-        if (array_key_exists($key, $this->settings)) { return $this->settings[$key]; }
+        if (array_key_exists($key, $this->settings)) { 
+			return $this->settings[$key];
+		}
+		
         return false;
     }
 
-    protected function getPath($name) {       return '\\League\CLImate\\Settings\\' . $this->getClassName($name);}
+    protected function getPath($name) {
+		return '\\League\CLImate\\Settings\\' . $this->getClassName($name);
+	}
 
-    protected function getClassName($name)
-    {return ucwords(str_replace('add_', '', $name));}
+    protected function getClassName($name) {
+		return ucwords(str_replace('add_', '', $name));
+	}
 
-	public function settings(){return [];}
+	public function settings(){
+		return [];
+	}
 
-    public function importSetting($setting)
-    {
-        $short_name = basename(str_replace('\\', '/', get_class($setting))); $method = 'importSetting' 
-		. $short_name;
-if (method_exists($this, $method))
-
- {
-            $this->$method($setting);}
-
-    }
+    public function importSetting($setting) {
+        $short_name = basename(str_replace('\\', '/', get_class($setting)));
+		$method = 'importSetting' . $short_name;
+		if (method_exists($this, $method)) {
+            $this->$method($setting);
+		}
+	}
 }
