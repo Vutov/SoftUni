24ff25548164a290327a9826d191052cc5b064d8
Spas Vutov <VutovSpas@gmail.com>
2015-05-18 16:24:09 +0300

    OOP 05.EncapsulationAndPolymophism 1/3 Done

---
+++ OOP/Practice/05.Encapsulation-and-Polymorphism/Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Shapes
+{
+    class Rectangle : BasicShape
+    {
+        public Rectangle(double width, double height)
+            : base(width, height)
+        {
+        }
+
+        public override double CalculateArea()
+        {
+            double area = this.Width * this.Height;
+
+            return area;
+        }
+
+        public override double CalculatePerimether()
+        {
+            double p = this.Width * 2 + this.Height * 2;
+
+            return p;
+        }
+    }
+}
---
+++ OOP/Practice/05.Encapsulation-and-Polymorphism/Shapes/Shapes/Triangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes.Shapes
+{
+    class Triangle : BasicShape
+    {
+        private double sideC;
+
+        public double SideC
+        {
+            get { return this.sideC; }
+            set
+            {
+                if (IsValidSize(value))
+                {
+                    this.sideC = value;
+                }
+            }
+        }
+
+        public Triangle(double sideA, double sideB, double sideC)
+            : base(sideA, sideB)
+        {
+            this.SideC = sideC;
+        }
+
+        /// <summary>
+        /// Calculates area of the triangle, if the given sides cannot make triangle
+        /// return NaN.
+        /// </summary>
+        /// <returns>double area or NaN</returns>
+        public override double CalculateArea()
+        {
+            double halfPerimeter = this.CalculatePerimether() / 2;
+
+            double areaSquared = halfPerimeter;
+            areaSquared *= halfPerimeter - this.Width;
+            areaSquared *= halfPerimeter - this.Height;
+            areaSquared *= halfPerimeter - this.SideC;
+
+            double area = Math.Sqrt(areaSquared);
+
+            return area;
+        }
+
+        public override double CalculatePerimether()
+        {
+            double P = this.Width + this.Height + this.sideC;
+
+            return P;
+        }
+    }
+}
