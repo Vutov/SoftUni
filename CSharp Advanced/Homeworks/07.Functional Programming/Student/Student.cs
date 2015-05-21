58c50b85b1c035370f142ba5131231af42b7d914
Spas Vutov <VutovSpas@gmail.com>
2015-05-21 19:29:47 +0300

    C#A 07.Functional Programming Homework Done

---
+++ CSharp Advanced/Homeworks/07.Functional Programming/Student/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string FacultyNumber { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public List<int> Marks { get; set; }
+        public int GroupNumber { get; set; }
+
+        public string GroupName { get; set; }
+
+        public Student(string firstName, string lastName, int age,
+            string facultyNumber, string phone, string email,
+            List<int> marks, int groupNumber, string groupName = "No Group")
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.FacultyNumber = facultyNumber;
+            this.Phone = phone;
+            this.Email = email;
+            this.Marks = marks;
+            this.GroupNumber = groupNumber;
+            this.GroupName = groupName;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", this.FirstName, this.LastName);
+        }
+    }
+}
