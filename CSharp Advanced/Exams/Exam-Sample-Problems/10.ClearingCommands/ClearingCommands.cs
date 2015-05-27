570bec62441bae1ce764470250bea93eb8d4f23e
Spas Vutov <VutovSpas@gmail.com>
2015-05-27 21:57:07 +0300

    C#A Example exam problems 10 and 21

---
+++ CSharp Advanced/Exams/Exam-Sample-Problems/10.ClearingCommands/ClearingCommands.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _10.ClearingCommands
+{
+    class ClearingCommands
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var field = new List<List<char>>();
+            var nonClearSymbols = new[] { '>', '<', 'v', '^' };
+
+            while (!input.Equals("END"))
+            {
+                field.Add(input.ToCharArray().ToList());
+
+                input = Console.ReadLine();
+            }
+
+            for (int row = 0; row < field.Count; row++)
+            {
+                for (int col = 0; col < field[row].Count; col++)
+                {
+                    char currentSymbol = field[row][col];
+                    var currentCol = col;
+                    var currentRow = row;
+                    var nextChar = ' ';
+                    switch (currentSymbol)
+                    {
+                        case '>':
+                            currentCol++;
+                            if (currentCol < field[row].Count)
+                            {
+                                nextChar = field[row][currentCol];
+                                while (!nonClearSymbols.Contains(nextChar))
+                                {
+                                    field[row][currentCol] = ' ';
+                                    if (currentCol < field[row].Count - 1)
+                                    {
+                                        currentCol++;
+                                        nextChar = field[row][currentCol];
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                        case '<':
+                            currentCol--;
+                            if (currentCol >= 0)
+                            {
+                                nextChar = field[row][currentCol];
+                                while (!nonClearSymbols.Contains(nextChar))
+                                {
+                                    field[row][currentCol] = ' ';
+                                    if (currentCol > 0)
+                                    {
+                                        currentCol--;
+                                        nextChar = field[row][currentCol];
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                        case '^':
+                            currentRow--;
+                            if (currentRow >= 0)
+                            {
+                                nextChar = field[currentRow][col];
+                                while (!nonClearSymbols.Contains(nextChar))
+                                {
+                                    field[currentRow][col] = ' ';
+                                    if (currentRow > 0)
+                                    {
+                                        currentRow--;
+                                        nextChar = field[currentRow][col];
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                        case 'v':
+                            currentRow++;
+                            if (currentRow < field.Count - 1)
+                            {
+                                nextChar = field[currentRow][col];
+                                while (!nonClearSymbols.Contains(nextChar))
+                                {
+                                    field[currentRow][col] = ' ';
+                                    if (currentRow < field.Count - 1)
+                                    {
+                                        currentRow++;
+                                        nextChar = field[currentRow][col];
+                                    }
+                                    else
+                                    {
+                                        break;
+                                    }
+                                }
+                            }
+
+                            break;
+                    }
+                }
+            }
+
+            foreach (var row in field)
+            {
+                var line = string.Join("", row);
+                line = line.Replace(">", "&gt;");
+                line = line.Replace("<", "&lt;");
+                Console.WriteLine("<p>{0}</p>", line);
+            }
+        }
+    }
+}
---
+++ CSharp Advanced/Exams/Exam-Sample-Problems/21.Oh, My Girl!/OhMyGirl.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _21.Oh__My_Girl_
+{
+    class OhMyGirl
+    {
+        static void Main(string[] args)
+        {
+           
+            var key = Console.ReadLine();
+            var text = Console.ReadLine();
+            var allText = "";
+            while (!text.Equals("END"))
+            {
+                allText += text;
+                text = Console.ReadLine();
+            }
+
+            var digitRegex = @"\d*";
+            var lowerCaseRegex = @"[a-z]*";
+            var upperCaseRegex = @"[A-Z]*";
+            var capture = @"(.{2,6})";
+            var keyRegex = Regex.Escape(key[0].ToString());
+            for (int i = 1; i < key.Length - 1; i++)
+            {
+                if (key[i] >= 'a' && key[i] <= 'z')
+                {
+                    keyRegex += lowerCaseRegex;
+                }
+                else if (key[i] >= 'A' && key[i] <= 'Z')
+                {
+                    keyRegex += upperCaseRegex;
+                } 
+                else if (key[i] >= '0' && key[i] <= '9')
+                {
+                    keyRegex += digitRegex;
+                }
+                else
+                {
+                    keyRegex += Regex.Escape(key[i].ToString());
+                }
+            }
+
+            keyRegex += Regex.Escape(key[key.Length - 1].ToString());
+            var regex = keyRegex + capture + keyRegex;
+
+            var matches = Regex.Matches(allText, regex);
+            var message = string.Empty;
+            foreach (Match match in matches)
+            {
+                message += match.Groups[1];
+            }
+
+            Console.WriteLine(message);
+        }
+    }
+}
