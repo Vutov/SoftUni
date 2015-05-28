f5395aa7cb206eb44ea16bb0df4a561ef2f9b4a3
Spas Vutov <VutovSpas@gmail.com>
2015-05-28 19:45:24 +0300

    C#A Example exam problem 11 and 12

---
+++ CSharp Advanced/Exams/Exam-Sample-Problems/12.PhoneNumbers/PhoneNumbers.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _12.PhoneNumbers
+{
+    class PhoneNumbers
+    {
+        static void Main(string[] args)
+        {
+            var regex = @"([A-Z]{1,}[a-zA-Z]*)[^a-zA-Z\+]*?((?:\d+|\+\d)[\d\(\)/\.\- ]*\d+)";
+
+            var allText = string.Empty;
+            var text = Console.ReadLine();
+            while (!text.Equals("END"))
+            {
+                allText += text;
+                text = Console.ReadLine();
+            }
+
+            var foundPairs = Regex.Matches(allText, regex);
+            if (foundPairs.Count != 0)
+            {
+                Console.Write("<ol>");
+                foreach (Match foundPair in foundPairs)
+                {
+                    var number = Regex.Replace(foundPair.Groups[2].ToString(), @"[\(\)/\.\- ]+", "");
+                    Console.Write("<li><b>{0}:</b> {1}</li>", foundPair.Groups[1], number);
+                }
+                Console.Write("</ol>");
+            }
+            else
+            {
+                Console.WriteLine("<p>No matches!</p>");
+            }
+        }
+    }
+}
