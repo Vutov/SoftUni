0705c407e7900bb2623ffd6352a1105939e37ec1
Spas Vutov <VutovSpas@gmail.com>
2015-06-11 17:18:37 +0300

    OOP OtherTypes Class Excercise Done

---
+++ OOP/Excersice in Class/Custom List/CustomList/CustomList/GenericList.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class GenericList<T> where T: IComparable
+    {
+        private const int DefaultCapacity = 16;
+        private T[] elements;
+        private int currentIndex;
+
+        public GenericList(int capacity = DefaultCapacity)
+        {
+            this.elements = new T[capacity];
+            currentIndex = 0;
+        }
+
+        public void Add(T element)
+        {
+            if (currentIndex >= elements.Length)
+            {
+                this.Resize();
+            }
+
+            this.elements[currentIndex] = element;
+
+            currentIndex++;
+        }
+
+        public void Remove(T element)
+        {
+            int emptyIndex = elements.Length;
+
+            for (int i = 0; i < currentIndex; i++)
+            {
+                T currElement = this.elements[i];
+                if (currElement.Equals(element))
+                {
+                    this.elements[i] = default(T);
+                    emptyIndex = i;
+                }
+            }
+
+            if (!emptyIndex.Equals(elements.Length))
+            {
+                for (int i = emptyIndex; i < currentIndex; i++)
+                {
+                    T tmp = this.elements[i];
+                    this.elements[i] = this.elements[i + 1];
+                    this.elements[i + 1] = tmp;
+                }
+            }
+
+            currentIndex--;
+        }
+
+        public T Min()
+        {
+            T min = this.elements[0];
+
+            for (int i = 1; i < currentIndex; i++)
+            {
+                T currElement = this.elements[i];
+                if (currElement.CompareTo(min) < 0)
+                {
+                    min = currElement;
+                }
+            }
+
+            return min;
+        }
+
+        public T Max()
+        {
+            T max = this.elements[0];
+
+            for (int i = 1; i < currentIndex; i++)
+            {
+                T currElement = this.elements[i];
+                if (currElement.CompareTo(max) > 0)
+                {
+                    max = currElement;
+                }
+            }
+
+            return max;
+        }
+
+        public int Count()
+        {
+            return currentIndex;
+        }
+
+        public int Capacity()
+        {
+            return this.elements.Length;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return this.elements[index];
+            }
+            set
+            {
+                this.elements[index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", this.elements.Take(currentIndex));
+        }
+
+        public bool Contains(T element)
+        {
+            for (int i = 0; i < currentIndex; i++)
+            {
+                T currElement = this.elements[i];
+                if (currElement.Equals(element))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Sort()
+        {
+            bool sort = true;
+            while (sort)
+            {
+                sort = false;
+                for (int i = 0; i < currentIndex - 1; i++)
+                {
+                    if (this.elements[i].CompareTo(this.elements[i + 1]) > 0)
+                    {
+                        T swapper = this.elements[i];
+                        this.elements[i] = this.elements[i + 1];
+                        this.elements[i + 1] = swapper;
+                        sort = true;
+                    }
+                }
+            }
+        }
+
+        public void Reverse()
+        {
+            var revElemenets = new T[currentIndex];
+            int index = 0;
+            for (int i = currentIndex - 1; i >= 0; i--)
+            {
+                revElemenets[index] = this.elements[i];
+                index++;
+            }
+
+            this.elements = revElemenets;
+        }
+
+        private void Resize()
+        {
+            T[] resized = new T[elements.Length * 2];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                resized[i] = this.elements[i];
+            }
+
+            this.elements = resized;
+        }
+    }
+}
---
+++ OOP/Excersice in Class/Custom List/CustomList/CustomList/Tests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    class Tests
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> myList = new GenericList<int>();
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(15);
+            myList.Add(6);
+            myList.Add(1);
+            myList.Add(1);
+            myList.Add(1);
+            myList.Add(1);
+            myList.Add(1);
+            myList.Add(1);
+            myList.Add(6);
+            myList.Add(6);
+            myList.Add(6);
+            myList.Add(6);
+            myList.Add(6);
+            myList.Add(6);
+            myList.Add(-1);
+
+            Console.WriteLine(myList.Max());
+            Console.WriteLine(myList.Min());
+            Console.WriteLine(myList[3]);
+
+            myList.Remove(15);
+
+            myList[0] = 50;
+
+            myList.Sort();
+
+            Console.WriteLine(myList);
+
+            myList.Reverse();
+
+            Console.WriteLine(myList);
+
+            Console.WriteLine(myList.Contains(1000));
+        }
+    }
+}
