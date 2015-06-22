30aab8f5a9fe808ec6e0e3c56549e43dd0dce156
Spas Vutov <VutovSpas@gmail.com>
2015-06-22 20:12:00 +0300

    OOP Other Types - Class exercise Done

---
+++ OOP/Excersice in Class/Common Type System/Country/Country.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country
+{
+    class Country : ICloneable, IComparable
+    {
+        private string name;
+        private long population;
+        private double area;
+        private IList<string> cities;
+
+        public Country(string name, long population, double area, IList<string> cities)
+        {
+            this.Name = name;
+            this.Population = population;
+            this.Area = area;
+            this.Cities = cities;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+
+            }
+            set
+            {
+                if (value.Length != 0)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Name cannot be empty!");
+                }
+            }
+        }
+
+        public long Population
+        {
+            get
+            {
+                return this.population;
+
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.population = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Population cannot be negative!");
+                }
+            }
+        }
+
+        public double Area
+        {
+            get
+            {
+                return this.area;
+
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    this.area = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Area cannot be 0 or below!");
+                }
+            }
+        }
+
+        public IList<string> Cities { get; set; }
+
+        public object Clone()
+        {
+            return new Country(this.Name, this.Population, this.Area, new List<string>(this.Cities));
+        }
+
+        public int CompareTo(object other)
+        {
+            var country = other as Country;
+            if (country != null)
+            {
+                if (country.Area > this.Area)
+                {
+                    return 1;
+                }
+
+                if (country.Population > this.Population)
+                {
+                    return 1;
+                }
+
+                return country.Name.CompareTo(this.Name);
+            }
+
+            return 0;
+        }
+
+        public override bool Equals(object other)
+        {
+            
+            if (other is Country)
+            {
+                var country = other as Country;
+                if (country.Name == this.Name)
+                {
+                    return true;
+                }
+            }
+
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Population.GetHashCode() ^ this.Population.GetHashCode();
+        }
+
+        public static bool operator ==(Country first, Country second)
+        {
+            return first.Equals(second);
+        }
+
+        public static bool operator !=(Country first, Country second)
+        {
+            return !first.Equals(second);
+        }
+    }
+}
---
+++ OOP/Excersice in Class/Common Type System/Country/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Country
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Country bg = new Country("Bulgaria", 7100000, 111000, new List<string>() {"Sofia", "Plovdiv", "Varna"});
+            Country usa = new Country("USA", 300000000, 1200000, new List<string>(){ "New York", "Los Angeles", "San Francisco"});
+            Country bg2 = new Country("Bulgaria", 8000000, 10, new List<string>());
+            Country bg3 = new Country("Bulgaria", 8000000, 111000, new List<string>());
+            Country hr = new Country("Croatia", 8000000, 111000, new List<string>());
+
+            Console.WriteLine(bg == bg2); // True
+            Console.WriteLine(bg == usa); // False
+            Console.WriteLine(bg != bg2); // False
+            Console.WriteLine(bg != usa); // True
+
+            var countries = new List<Country> { bg, usa, bg2, bg3, hr };
+            countries.Sort();
+
+            Console.WriteLine(
+                string.Join(Environment.NewLine, countries.Select(c => new { c.Name, c.Area, c.Population })));
+
+            var bgCopy = bg.Clone() as Country;
+            bg.Cities.Add("Kaspichan");
+            Console.WriteLine(string.Join(", ", bg.Cities));
+            Console.WriteLine(string.Join(", ", bgCopy.Cities));
+        }
+    }
+}
