0a66ff727c5095a0c567ea2dd901d90f72e98e05
Spas Vutov <VutovSpas@gmail.com>
2015-07-13 19:40:42 +0300

    HQC 03.Code Formatting Homework Done

---
+++ HQC/Homeworks/03. Code-Formatting-Homework/Event/Event.cs
@@ -0,0 +1,235 @@
+﻿namespace Event
+{
+    using System;
+    using System.Text;
+
+    public class Event : IComparable
+    {
+        private readonly string title;
+        private readonly string location;
+        private DateTime date;
+
+        public Event(DateTime date, string title, string location)
+        {
+            this.date = date;
+            this.title = title;
+            this.location = location;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Event other = obj as Event;
+            int compareByDate = this.date.CompareTo(other.date);
+            int compareByTitle = this.title.CompareTo(other.title);
+            int compareByLocation = this.location.CompareTo(other.location);
+
+            if (compareByDate == 0)
+            {
+                if (compareByTitle == 0)
+                {
+                    return compareByLocation;
+                }
+
+                return compareByTitle;
+            }
+
+            return compareByDate;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(this.date.ToString("yyyy-MM-ddTHH:mm:ss"));
+            stringBuilder.Append(" | " + this.title);
+            if (!string.IsNullOrEmpty(this.location))
+            {
+                stringBuilder.Append(" | " + this.location);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    public class EventHandler
+    {
+        public static readonly StringBuilder Output = new StringBuilder();
+
+        public static void Main(string[] args)
+        {
+            while (ExecuteNextCommand())
+            {
+                // Do magic here.
+            }
+
+            Console.WriteLine(Output);
+        }
+
+        private static bool ExecuteNextCommand()
+        {
+            string command = Console.ReadLine();
+            if (command[0] == 'A')
+            {
+                AddEvent(command);
+                return true;
+            }
+
+            if (command[0] == 'D')
+            {
+                DeleteEvents(command);
+                return true;
+            }
+
+            if (command[0] == 'L')
+            {
+                ListEvents(command);
+                return true;
+            }
+
+            if (command[0] == 'E')
+            {
+                return false;
+            }
+
+            return false;
+        }
+
+        private static void ListEvents(string command)
+        {
+            int pipeIndex = command.IndexOf('|');
+            DateTime date = GetDate(command, "ListEvents");
+            string countString = command.Substring(pipeIndex + 1);
+            int count = int.Parse(countString);
+            Events.ListEvents(date, count);
+        }
+
+        private static void DeleteEvents(string command)
+        {
+            string title = command.Substring("DeleteEvents".Length + 1);
+            Events.DeleteEvents(title);
+        }
+
+        private static void AddEvent(string command)
+        {
+            DateTime date;
+            string title;
+            string location;
+            GetParameters(command, "AddEvent", out date, out title, out location);
+            Events.AddEvent(date, title, location);
+        }
+
+        private static void GetParameters(
+            string commandForExecution,
+            string commandType,
+            out DateTime dateAndTime,
+            out string eventTitle,
+            out string eventLocation)
+        {
+            dateAndTime = GetDate(commandForExecution, commandType);
+            int firstPipeIndex = commandForExecution.IndexOf('|');
+            int lastPipeIndex = commandForExecution.LastIndexOf('|');
+            if (firstPipeIndex == lastPipeIndex)
+            {
+                eventTitle = commandForExecution.Substring(firstPipeIndex + 1).Trim();
+                eventLocation = string.Empty;
+            }
+            else
+            {
+                eventTitle =
+                    commandForExecution.Substring(firstPipeIndex + 1, lastPipeIndex - firstPipeIndex - 1).Trim();
+                eventLocation = commandForExecution.Substring(lastPipeIndex + 1).Trim();
+            }
+        }
+
+        private static DateTime GetDate(string command, string commandType)
+        {
+            DateTime date = DateTime.Parse(command.Substring(commandType.Length + 1, 20));
+            return date;
+        }
+
+        public static class Messages
+        {
+            public static void EventAdded()
+            {
+                Output.Append("Event added\n");
+            }
+
+            public static void EventDeleted(int x)
+            {
+                if (x == 0)
+                {
+                    NoEventsFound();
+                }
+                else
+                {
+                    Output.AppendFormat("{0} events deleted\n", x);
+                }
+            }
+
+            public static void NoEventsFound()
+            {
+                Output.Append("No events found\n");
+            }
+
+            public static void PrintEvent(Event eventToPrint)
+            {
+                if (eventToPrint != null)
+                {
+                    Output.Append(eventToPrint + "\n");
+                }
+            }
+        }
+
+        public class EventHolder
+        {
+            private static readonly EventHolder Events = new EventHolder();
+
+            private readonly MultiDictionary<string, Event> eventByTitle = new MultiDictionary<string, Event>(true);
+
+            private readonly OrderedBag<Event> eventByDate = new OrderedBag<Event>();
+
+            public void AddEvent(DateTime date, string title, string location)
+            {
+                Event newEvent = new Event(date, title, location);
+                this.eventByTitle.Add(title.ToLower(), newEvent);
+                this.eventByDate.Add(newEvent);
+                Messages.EventAdded();
+            }
+
+            public void DeleteEvents(string titleToDelete)
+            {
+                string title = titleToDelete.ToLower();
+                int removed = 0;
+                foreach (var eventToRemove in this.eventByTitle[title])
+                {
+                    removed++;
+                    this.eventByDate.Remove(eventToRemove);
+                }
+
+                this.eventByTitle.Remove(title);
+                Messages.EventDeleted(removed);
+            }
+
+            public void ListEvents(DateTime date, int count)
+            {
+                OrderedBag<Event>.View eventsToShow =
+                    this.eventByDate.RangeFrom(new Event(date, string.Empty, string.Empty), true);
+                int showed = 0;
+                foreach (var eventToShow in eventsToShow)
+                {
+                    if (showed == count)
+                    {
+                        break;
+                    }
+
+                    Messages.PrintEvent(eventToShow);
+                    showed++;
+                }
+
+                if (showed == 0)
+                {
+                    Messages.NoEventsFound();
+                }
+            }
+        }
+    }
+}
---
+++ HQC/Homeworks/03. Code-Formatting-Homework/Reformated own code/JoinListsReformatted.cs
@@ -0,0 +1,51 @@
+﻿namespace Reformated_own_code
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class JoinListsReformatted
+    {
+        public static void Main(string[] args)
+        {
+            string firstIntegers = Console.ReadLine();
+            List<string> finalNumbers = firstIntegers.Split(' ').ToList();
+            string secondIntegers = Console.ReadLine();
+            string[] secondArray = secondIntegers.Split(' ');
+
+            foreach (string number in secondArray)
+            {
+                if (finalNumbers.IndexOf(number) == -1)
+                {
+                    finalNumbers.Add(number);
+                }
+            }
+
+            int len = finalNumbers.Count;
+            for (int i = 0; i < len; i++)
+            {
+                while (finalNumbers.IndexOf(finalNumbers[i]) != finalNumbers.LastIndexOf(finalNumbers[i]))
+                {
+                    finalNumbers.Remove(finalNumbers[i]);
+                    len--;
+                }
+            }
+
+            // int sorting needed, string sorting does 2, 22, 23, 4, 43, etc.
+            List<int> digits = new List<int>();
+            foreach (string number in finalNumbers)
+            {
+                digits.Add(Convert.ToInt32(number));
+            }
+
+            digits.Sort();
+
+            foreach (int number in digits)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
