37e28c0e2103471bc7309c1ad12cdcffdf20753c
Spas Vutov <VutovSpas@gmail.com>
2015-07-15 01:24:39 +0300

    DBA 01.Intro Class Exercise HalfAss Done

---
+++ DataBase Applications/Class Exersice/01.Intro excerside/01.Db/Emplyees.cs
@@ -0,0 +1,51 @@
+﻿namespace _01.Db
+{
+    using System;
+    using System.Linq;
+
+    public class Emplyees
+    {
+        public static void Main(string[] args)
+        {
+            var context = new SoftUniEntities();
+
+            var employeeNames = context.Employees
+                .Where(e => e.Salary > 50000)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ToList();
+            employeeNames.ForEach(Console.WriteLine);
+
+            var employeesFromSeattle = context.Employees
+                .Where(e => e.Department.Name == "Research and Development")
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName)
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.Department.Name,
+                    e.Salary
+                })
+                .ToList();
+
+            employeesFromSeattle.ForEach(e => Console.WriteLine("{0} {1} from {2} - ${3}",
+                e.FirstName, e.LastName, e.Name, e.Salary));
+
+            var address = new Address()
+            {
+                AddressText = "Vitoshka 15",
+                TownID = 4
+            };
+
+            context.Addresses.Add(address);
+
+            context.SaveChanges();
+        }
+    }
+}
