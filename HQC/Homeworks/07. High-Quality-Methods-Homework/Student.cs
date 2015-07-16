fe7f992d424fe8485a726cab91ff72bc22a7f04f
Spas Vutov <VutovSpas@gmail.com>
2015-07-16 14:09:14 +0300

    HQC 07. HQ Methods Homework Done

---
+++ HQC/Homeworks/07. High-Quality-Methods-Homework/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherInfo { get; set; }
+
+        public Student(string firstName, string lastName, string otherInfo)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.OtherInfo = otherInfo;
+        }
+
+        public bool IsOlderThan(Student other)
+        {
+            DateTime firstDate = this.GetDateFromInfo(this.OtherInfo);
+            DateTime secondDate = this.GetDateFromInfo(other.OtherInfo);
+
+            return firstDate > secondDate;
+        }
+
+        private DateTime GetDateFromInfo(string info)
+        {
+            var date = DateTime.Parse(info.Substring(info.Length - 10));
+
+            return date;
+        }
+    }
+}
