e3b2d662a548b2d001d89abf1344e5708d90a8ad
Spas Vutov <VutovSpas@gmail.com>
2015-07-16 20:20:43 +0300

    DS 04.Trees Class Exersice Done

---
+++ Data Structures/Class Exersice/04.Implement Trees and Traversals/Binary-Tree/BinaryTree.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+public class BinaryTree<T>
+{
+    public BinaryTree(T value, BinaryTree<T> leftChild = null, BinaryTree<T> rightChild = null)
+    {
+        this.Value = value;
+        this.LeftChild = leftChild;
+        this.RightChild = rightChild;
+    }
+
+    public T Value { get; set; }
+
+    public BinaryTree<T> LeftChild { get; set; }
+    public BinaryTree<T> RightChild { get; set; }
+
+    public void PrintIndentedPreOrder(int depth = 0)
+    {
+        Console.Write(new string(' ', 2 * depth));
+        Console.WriteLine(this.Value);
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.PrintIndentedPreOrder(depth + 1);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.PrintIndentedPreOrder(depth + 1);   
+        }
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachInOrder(action);
+        }
+
+        action(this.Value);
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachInOrder(action);
+        }
+    }
+
+    public void EachPostOrder(Action<T> action)
+    {
+        if (this.LeftChild != null)
+        {
+            this.LeftChild.EachPostOrder(action);
+        }
+
+        if (this.RightChild != null)
+        {
+            this.RightChild.EachPostOrder(action);
+        }
+
+        action(this.Value);
+    }
+}
---
+++ Data Structures/Class Exersice/04.Implement Trees and Traversals/Binary-Tree/Tree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Tree<T>
+{
+    public Tree(T value, params Tree<T>[] children)
+    {
+        this.Value = value;
+        this.Children = new List<Tree<T>>();
+        foreach (var child in children)
+        {
+            this.Children.Add(child);
+        }
+    }
+
+    public T Value { get; private set; }
+
+    public IList<Tree<T>> Children { get; private set; }
+
+    public void Print(int depth = 0)
+    {
+        Console.Write(new string(' ', 2 * depth));
+        Console.WriteLine(this.Value);
+        foreach (var child in this.Children)
+        {
+            child.Print(depth + 1);
+        }
+    }
+
+    public void Each(Action<T> action)
+    {
+        action(this.Value);
+
+        foreach (var child in this.Children)
+        {
+            child.Each(action);
+        }
+    }
+}
