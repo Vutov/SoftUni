23abaf11616007429729a7518356de345130b56c
Spas Vutov <VutovSpas@gmail.com>
2015-07-20 20:40:38 +0300

    DS 04.Trees Homework Done

---
+++ Data Structures/Homeworks/04.Trees and Tree-Like Data Structures/02.DirectoryContent/DirectoryContent.cs
@@ -0,0 +1,17 @@
+﻿namespace _02.DirectoryContent
+{
+    using System;
+    using System.IO;
+
+    public class DirectoryContent
+    {
+        public static void Main(string[] args)
+        {
+            // Feel free to change to C:\\Windows\ , but it might be restricted.
+            var rootFolderInfo = new DirectoryInfo(@"..\..\..\");
+            Console.WriteLine(rootFolderInfo.FullName);
+            var directoryTree = new DirectoryTree(rootFolderInfo);
+            Console.WriteLine("{0} KB", directoryTree.GetSummedUpSize() / 1024);
+        }
+    }
+}
---
+++ Data Structures/Homeworks/04.Trees and Tree-Like Data Structures/02.DirectoryContent/DirectoryTree.cs
@@ -0,0 +1,40 @@
+﻿namespace _02.DirectoryContent
+{
+    using System.IO;
+    using System.Linq;
+    using System.Numerics;
+
+    public class DirectoryTree
+    {
+        private readonly DirectoryInfo rootFolder;
+
+        public DirectoryTree(DirectoryInfo rootFolder)
+        {
+            this.rootFolder = rootFolder;
+        }
+
+        public BigInteger GetSummedUpSize()
+        {
+            var size = this.GetSizeOfFolder(this.rootFolder, 0);
+
+            return size;
+        }
+
+        /// <summary>
+        /// Gets the full size of given folder using recursion.
+        /// </summary>
+        /// <param name="folder">Folder as DirectoryInfo</param>
+        /// <param name="currentSize">Current size in bytes</param>
+        /// <returns>All size in bytes</returns>
+        private BigInteger GetSizeOfFolder(DirectoryInfo folder, BigInteger currentSize)
+        {
+            currentSize += folder.GetFiles().Sum(f => f.Length);
+            foreach (var child in folder.GetDirectories())
+            {
+                currentSize = this.GetSizeOfFolder(child, currentSize);
+            }
+
+            return currentSize;
+        }
+    }
+}
