378653cd2419f87a9464733221389ec0296aad4e
Spas Vutov <VutovSpas@gmail.com>
2015-07-27 14:33:46 +0300

    DS 05. DFS and DFS Exsercise Done

---
+++ Data Structures/Class Exersice/5. DFS-and-BFS/BFS-Escape-from-Labyrinth.Tests/UnitTestsEscapeFromLabyrinth.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+[TestClass]
+public class UnitTestsEscapeFromLabyrinth
+{
+    [TestMethod]
+    public void TestLabyrinth9x7()
+    {
+        // Arrange
+        var input =
+            "9" + "\n" +
+            "7" + "\n" +
+            "*********" + "\n" +
+            "*----**--" + "\n" +
+            "**-*----*" + "\n" +
+            "*--*-*-**" + "\n" +
+            "*s*--*-**" + "\n" +
+            "**------*" + "\n" +
+            "*******-*" + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            EscapeFromLabyrinth.Main();
+        }
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "Shortest exit: URUURRDRRRUR";
+        output = output.Replace("\r\n", "\n").Trim();
+        Assert.AreEqual(expectedOutput, output);
+    }
+
+    [TestMethod]
+    public void TestLabyrinth4x3()
+    {
+        // Arrange
+        var input =
+            "4" + "\n" +
+            "3" + "\n" +
+            "****" + "\n" +
+            "*-s*" + "\n" +
+            "****" + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            EscapeFromLabyrinth.Main();
+        }
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "No exit!";
+        output = output.Replace("\r\n", "\n").Trim();
+        Assert.AreEqual(expectedOutput, output);
+    }
+
+    [TestMethod]
+    public void TestLabyrinth4x2()
+    {
+        // Arrange
+        var input =
+            "4" + "\n" +
+            "2" + "\n" +
+            "****" + "\n" +
+            "***s" + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            EscapeFromLabyrinth.Main();
+        }
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "Start is at the exit.";
+        output = output.Replace("\r\n", "\n").Trim();
+        Assert.AreEqual(expectedOutput, output);
+    }
+
+
+    [TestMethod]
+    public void TestLabyrinth2x2()
+    {
+        // Arrange
+        var input =
+            "2" + "\n" +
+            "2" + "\n" +
+            "**" + "\n" +
+            "**" + "\n";
+
+        // Act
+        var inputReader = new StringReader(input);
+        var outputWriter = new StringWriter();
+        using (outputWriter)
+        {
+            Console.SetIn(inputReader);
+            Console.SetOut(outputWriter);
+            EscapeFromLabyrinth.Main();
+        }
+        var output = outputWriter.ToString();
+
+        // Assert
+        var expectedOutput = "No exit!";
+        output = output.Replace("\r\n", "\n").Trim();
+        Assert.AreEqual(expectedOutput, output);
+    }
+}
---
+++ Data Structures/Class Exersice/5. DFS-and-BFS/DFS-Graph-Traversal/GraphConnectedComponents.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class GraphConnectedComponents
+{
+    private static List<int>[] graph;
+
+    //new List<int>() {3, 6},
+    //new List<int>() {3, 4, 5, 6},
+    //new List<int>() {8},
+    //new List<int>() {0, 1, 5},
+    //new List<int>() {1, 6},
+    //new List<int>() {1, 6},
+    //new List<int>() {1, 3},
+    //new List<int>() {},
+    //new List<int>() {2}
+
+    private static bool[] visited;
+
+    public static void Main()
+    {
+        ReadGraph();
+        FindGraphConnectedComponents();
+    }
+
+    private static void DFS(int index)
+    {
+        if (!visited[index])
+        {
+            visited[index] = true;
+
+            var node = graph[index];
+            foreach (var neighbor in node)
+            {
+
+                DFS(neighbor);
+            }
+
+            Console.Write(" " + index);
+        }
+    }
+
+    private static void FindGraphConnectedComponents()
+    {
+        visited = new bool[graph.Length];
+        for (int i = 0; i < graph.Length; i++)
+        {
+            if (!visited[i])
+            {
+                Console.Write("Connected component:");
+                DFS(i);
+                Console.WriteLine();
+            }
+        }
+    }
+
+    private static void ReadGraph()
+    {
+        var n = int.Parse(Console.ReadLine());
+        graph = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            var node = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            graph[i] = node;
+        }
+    }
+}
