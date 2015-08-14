59feca841624651e8e48164e81b056de1d00caed
Spas Vutov <VutovSpas@gmail.com>
2015-08-14 22:43:25 +0300

    DS Data Collections Class Exersice Done

---
+++ Data Structures/Class Exersice/7. Collection-Data-Structures-and-Libraries-Exercises/01.EventsInGivenRange/EventsInGivenRange.cs
@@ -0,0 +1,43 @@
+﻿namespace _01.EventsInGivenRange
+{
+    using System;
+    using System.Globalization;
+    using System.Threading;
+    using Wintellect.PowerCollections;
+
+    public class EventsInGivenRange
+    {
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+
+            var eventCount = int.Parse(Console.ReadLine());
+            var datesAndEvents = new OrderedMultiDictionary<DateTime, string>(true);
+            for (int i = 0; i < eventCount; i++)
+            {
+                var currentEvent = Console.ReadLine();
+                var token = currentEvent.Split('|');
+                var date = DateTime.Parse(token[1].Trim());
+                var name = token[0].Trim();
+                datesAndEvents.Add(date, name);
+            }
+
+            var queryNumber = int.Parse(Console.ReadLine());
+            for (int i = 0; i < queryNumber; i++)
+            {
+                var query = Console.ReadLine().Split('|');
+                var startDate = DateTime.Parse(query[0].Trim());
+                var endDate = DateTime.Parse(query[1].Trim());
+                var queryData = datesAndEvents.Range(startDate, true, endDate, true);
+                Console.WriteLine(queryData.KeyValuePairs.Count);
+                foreach (var e in queryData)
+                {
+                    foreach (var name in e.Value)
+                    {
+                        Console.WriteLine("{0} | {1:dd-MMM-yyyy}", name, e.Key);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data Structures/Class Exersice/7. Collection-Data-Structures-and-Libraries-Exercises/02.Rope for Efficient String Editing/StringEditing.cs
@@ -0,0 +1,67 @@
+﻿namespace _02.Rope_for_Efficient_String_Editing
+{
+    using System;
+    using System.Linq;
+    using Wintellect.PowerCollections;
+
+    public class StringEditing
+    {
+        public static void Main(string[] args)
+        {
+            var data = new BigList<char>();
+
+            Console.WriteLine("Give commands in format commans|value1|value2... etc");
+            var line = Console.ReadLine();
+            while (line != "Exit")
+            {
+                var tokens = line.Split('|').Select(c => c.Trim()).ToList();
+                var command = tokens[0];
+                switch (command)
+                {
+                    case "Insert":
+                        if (tokens.Count != 2)
+                        {
+                            throw new ArgumentException("Invalid command format!");
+                        }
+
+                        data.AddRangeToFront(tokens[1].ToCharArray());
+                        Console.WriteLine("OK");
+                        break;
+                    case "Append":
+                        if (tokens.Count != 2)
+                        {
+                            throw new ArgumentException("Invalid command format!");
+                        }
+
+                        data.AddRange(tokens[1].ToCharArray());
+                        Console.WriteLine("OK");
+                        break;
+                    case "Delete":
+                        if (tokens.Count != 3)
+                        {
+                            throw new ArgumentException("Invalid command format!");
+                        }
+
+                        data.RemoveRange(int.Parse(tokens[1]), int.Parse(tokens[2]));
+                        Console.WriteLine("OK");
+                        break;
+                    case "Print":
+                        if (tokens.Count != 1)
+                        {
+                            throw new ArgumentException("Invalid command format!");
+                        }
+
+                        foreach (var ch in data)
+                        {
+                             Console.Write(ch);
+                        }
+
+                        Console.WriteLine();
+                        break;
+                }
+
+                line = Console.ReadLine();
+            }
+        }
+    }
+}
