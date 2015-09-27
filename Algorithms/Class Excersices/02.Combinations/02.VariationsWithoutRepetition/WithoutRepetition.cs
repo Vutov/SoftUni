467aef5a4110ef28687bfbc0613fdbed3f6fcf9d
Spas Vutov <VutovSpas@gmail.com>
2015-09-27 15:26:13 +0300

    A 02.Combinations Class Exercise Done

---
+++ Algorithms/Class Excersices/02.Combinations/02.VariationsWithoutRepetition/WithoutRepetition.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.VariationsWithoutRepetition
+{
+    class WithoutRepetition
+    {
+        static void Main(string[] args)
+        {
+            int n = 3;//int.Parse(Console.ReadLine());
+            int k = 2;//int.Parse(Console.ReadLine());
+            var arr = new int[k];
+            var used = new bool[n + 1];
+
+            GenerateVariations(arr, used, n);
+        }
+
+        private static void GenerateVariations(int[] arr, bool[] used, int n, int index = 0)
+        {
+            if (index >= arr.Length)
+            {
+                Print(arr);
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (!used[i])
+                    {
+                        arr[index] = i;
+                        used[i] = true;
+                        GenerateVariations(arr, used, n, index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.WriteLine("({0})", string.Join(", ", arr));
+        }
+    }
+}
---
+++ Algorithms/Class Excersices/02.Combinations/04.CombinationsWithoutRepetition/CombinationsWihtoutRepetitions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.CombinationsWithoutRepetition
+{
+    class CombinationsWihtoutRepetitions
+    {
+        static void Main(string[] args)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[k];
+            bool[] used = new bool[n + 1];
+
+            GenerateVariations(arr, used, n);
+        }
+
+        private static void GenerateVariations(int[] arr, bool[] used, int n, int start = 1, int index = 0)
+        {
+            if (index >= arr.Length)
+            {
+                Print(arr);
+            }
+            else
+            {
+                for (int i = start; i <= n; i++)
+                {
+                    if (!used[i])
+                    {
+                        arr[index] = i;
+                        used[i] = true;
+                        GenerateVariations(arr, used, n, i, index + 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        private static void Print(int[] arr)
+        {
+            Console.WriteLine("({0})", string.Join(", ", arr));
+        }
+    }
+}
