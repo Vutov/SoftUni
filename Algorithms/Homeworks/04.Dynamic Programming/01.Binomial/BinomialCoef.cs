3fbb1d86b7d7baaa7586a9b6e7a01016dfd28807
Spas Vutov <VutovSpas@gmail.com>
2015-10-12 16:02:48 +0300

    A 04.Dynamic Programming Done

---
+++ Algorithms/Homeworks/04.Dynamic Programming/01.Binomial/BinomialCoef.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.Binomial
+{
+    using System;
+    using System.Collections.Generic;
+
+    class BinomialCoef
+    {
+        static void Main(string[] args)
+        {
+            var n = 10;
+            var k = 5;
+            var coef = new List<List<int>>
+            {
+                new List<int>() {1},
+                new List<int>() {1, 1, 0}
+            };
+
+            for (int row = 2; row <= n; row++)
+            {
+                coef.Add(new List<int>() { 1 });
+                for (int col = 1; col <= row; col++)
+                {
+                    var num = coef[row - 1][col - 1] + coef[row - 1][col];
+                    coef[row].Add(num);
+                }
+
+                coef[row].Add(0);
+            }
+
+            Console.WriteLine(coef[n][k]);
+        }
+    }
+}
