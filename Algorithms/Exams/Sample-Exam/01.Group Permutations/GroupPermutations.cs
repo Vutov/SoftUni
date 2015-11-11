39261b44ec0c7ad4b7db7d170adf6e716d3c43b5
Spas Vutov <VutovSpas@gmail.com>
2015-11-11 18:14:20 +0200

    A Sample Exam 2/4 Done

---
+++ Algorithms/Exams/Sample-Exam/01.Group Permutations/GroupPermutations.cs
@@ -0,0 +1,76 @@
+﻿namespace _01.Group_Permutations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    class GroupPermutations
+    {
+        private static readonly Dictionary<char, int> Letters = new Dictionary<char, int>();
+
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            var permutations = new List<char>();
+            foreach (var character in input)
+            {
+                if (!Letters.ContainsKey(character))
+                {
+                    Letters.Add(character, 0);
+                    permutations.Add(character);
+                }
+
+                Letters[character] += 1;
+            }
+
+            permutations.Sort();
+
+            PermuteRep(permutations, 0, permutations.Count);
+        }
+
+        public static void PermuteRep(IList<char> permutations, int start, int count)
+        {
+            Print(permutations);
+
+            if (start < count)
+            {
+                for (int i = count - 2; i >= start; i--)
+                {
+                    char swap;
+                    for (int j = i + 1; j < count; j++)
+                    {
+                        if (permutations[i] != permutations[j])
+                        {
+                            swap = permutations[i];
+                            permutations[i] = permutations[j];
+                            permutations[j] = swap;
+
+                            PermuteRep(permutations, i + 1, count);
+                        }
+                    }
+
+                    swap = permutations[i];
+                    for (int k = i; k < count - 1; )
+                    {
+                        permutations[k] = permutations[++k];
+                    }
+
+                    permutations[count - 1] = swap;
+                }
+            }
+        }
+
+        private static void Print(IEnumerable<char> permutations)
+        {
+            var result = new StringBuilder();
+            foreach (var permutation in permutations)
+            {
+                result.Append(new string(permutation, Letters[permutation]));
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Algorithms/Exams/Sample-Exam/02.Bridges/Bridges.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.Bridges
+{
+    class Bridges
+    {
+        static void Main(string[] args)
+        {
+            //var northSide = new List<int>() { 2, 5, 3, 3, 3, 1, 8, 2, 6, 7, 6 };
+            //var southSide = new List<int>() { 1, 2, 5, 3, 4, 1, 7, 8, 2, 5, 6 };
+            //var northSide = new List<int>() { 1 };
+            //var southSide = new List<int>() { 1, 1, 1 };
+            var northSide = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var southSide = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var field = new int[northSide.Count, southSide.Count];
+
+            if (northSide[0] == southSide[0])
+            {
+                field[0, 0] = 1;
+            }
+
+            for (int col = 1; col < southSide.Count; col++)
+            {
+                var currValue = field[0, col - 1];
+                if (northSide[0] == southSide[col])
+                {
+                    currValue++;
+                }
+
+                field[0, col] = currValue;
+            }
+
+            for (int row = 1; row < northSide.Count; row++)
+            {
+                var currValue = field[row - 1, 0];
+                if (northSide[row] == southSide[0])
+                {
+                    currValue++;
+                }
+
+                field[row, 0] = currValue;
+            }
+
+            for (int row = 1; row < northSide.Count; row++)
+            {
+                for (int col = 1; col < southSide.Count; col++)
+                {
+                    var currValue = Math.Max(field[row - 1, col], field[row, col - 1]);
+                    if (northSide[row] == southSide[col])
+                    {
+                        currValue++;
+                    }
+
+                    field[row, col] = currValue;
+                }
+            }
+
+
+            //for (int i = 0; i < northSide.Count; i++)
+            //{
+            //    for (int j = 0; j < southSide.Count; j++)
+            //    {
+            //        Console.Write(field[i, j] + " ");
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+            Console.WriteLine(field.Cast<int>().Max());
+        }
+    }
+}
